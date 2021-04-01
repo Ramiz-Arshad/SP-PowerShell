1e08c955d0ced25256b32b8189f4d8ae9e9ad2fd
Ramiz Arshad <ramizraja0123@gmail.com>
2021-04-01 08:14:29 +0300

    Add files via upload

--- Set Folder Permission/Set Folder Permission.ps1
+++ Set Folder Permission/Set Folder Permission.ps1
@@ -1,13 +1,16 @@
 ﻿$SiteURL = "https://actgroupqa.sharepoint.com/sites/DEV"
 Connect-PnPOnline –Url $SiteURL –Credentials (Get-Credential)
 
-$csv = Import-Csv "C:\SP\Add.csv"
+$csv = Import-Csv "C:\SP\Set Folder Permission\Add.csv"
 
 foreach($Row in $csv)
 {
+  $ListName = $Row.ListName  
   $FolderURL = $Row.Folder
   $GroupName = $Row.Group 
   $Permission = $Row.Permission
 
-  Set-PnPFolderPermission -Group $GroupName -Identity $FolderURL -List MyDocuments -AddRole $Permission
+  write-host $FolderURL "-" $GroupName "-" $Permission
+
+  Set-PnPFolderPermission -Group $GroupName -Identity $FolderURL -List $ListName -AddRole $Permission
 }
